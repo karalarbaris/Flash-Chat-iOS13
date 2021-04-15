115b837a8d0ca75dcd4c2db33a13e968f2bedce9
Baris <karalarbaris@gmail.com>
2021-04-15 18:33:09 +0200

    185. Registering New Users

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        	
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                // ...
+                if let e = error {
+                    print(e.localizedDescription)
+                    self.showAlert(message: e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
     
 }
--- Podfile
+++ Podfile
@@ -8,9 +8,18 @@ target 'Flash Chat iOS13' do
   # Pods for Flash Chat iOS13
   
   pod 'CLTypingLabel', '~> 0.4.0'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
   
   
-  
+end
+
+post_install do |installer|
+ installer.pods_project.targets.each do |target|
+  target.build_configurations.each do |config|
+   config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '9.0'
+  end
+ end
 end
 
 
