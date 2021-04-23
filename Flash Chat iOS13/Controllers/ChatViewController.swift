5b16cd9adaa23609bd3cf7d54fe3656cd8995228
Baris <karalarbaris@gmail.com>
2021-04-23 11:52:19 +0200

    197. Finishing Touches: UI and UX Improvements

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -47,6 +47,9 @@ class ChatViewController: UIViewController {
                     print("Database error \(e)")
                 } else {
                     print("Successfully saved data")
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
                 }
             }
         }
@@ -85,6 +88,9 @@ class ChatViewController: UIViewController {
                     //We are trying to manipulate user interface in closure so it's good practice to do
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
+                        let indexPath = IndexPath(row: self.messages.count-1, section: 0)
+                        self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                        
                     }
                 }
                 
@@ -103,9 +109,25 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let message = messages[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         //        cell.textLabel?.text = messages[indexPath.row].body
-        cell.label.text = messages[indexPath.row].body
+        cell.label.text = message.body
+        //This is a message from current user
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        } else {
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        
+       
         return cell
     }
     
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -19,6 +19,18 @@ class WelcomeViewController: UIViewController {
         titleLabel.text = K.appName
 
     }
+
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     
     func animateTitleLabel() {
         titleLabel.text = ""
